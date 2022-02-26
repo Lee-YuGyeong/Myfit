a79254d025b1c5ddf3d6cc110eb3afb77f4d8988
윤영미 <maie421@naver.com>
2022-02-26 22:08:31 +0900

    유저 회원가입 hash 처리

--- coupon/src/users/users.controller.spec.ts
+++ coupon/src/users/users.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UserRepository } from './users.repository';
 import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from 'src/dto/create-user.dto';
 
 describe('UsersController', () => {
   let userController: UsersController;
@@ -11,6 +13,7 @@ describe('UsersController', () => {
   const mockUserRepository = () => ({
     find: jest.fn(),
     findOne: jest.fn(),
+    save: jest.fn(),
   });
 
   
@@ -34,4 +37,27 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(userController).toBeDefined();
   });
+
+  describe('회원 저정', () => {
+    it('비밀 번호 해시 처리', async () => {
+      const passowrd_hash = 10;
+      const password = 'password';
+      const hash = await bcrypt.hash(password, passowrd_hash);
+
+      const isMatch = await bcrypt.compare(password, hash);
+
+      expect(isMatch).toBeTruthy();
+    });
+
+    it('회원 저장',()=>{
+      const dto_user = new CreateUserDto();
+
+      dto_user.password = '123';
+      dto_user.username = '테스트 유저';
+      
+      expect(userController.save(dto_user)).not.toEqual(null);
+
+    })
+  });
+
 });
--- coupon/src/users/users.controller.ts
+++ coupon/src/users/users.controller.ts
@@ -2,9 +2,13 @@ import { Controller, Get, UseGuards ,Request, Post, Body} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateUserDto } from 'src/dto/create-user.dto';
 import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
+
+const passowrd_hash = 10;
 
 @Controller('users')
 export class UsersController {
+
     constructor(
         private readonly userservice: UsersService
       ){}
@@ -17,6 +21,30 @@ export class UsersController {
 
     @Post() 
     async save(@Body() createUserDto: CreateUserDto){
-        return this.userservice.save(createUserDto);
+      const hash_password = await this.change_hash(createUserDto.password);
+      createUserDto.password = hash_password;
+      return this.userservice.save(createUserDto);
     }
+
+
+    @Get() 
+    async findAll(){
+      return this.userservice.findAll();
+    }
+
+    
+  private async check_hash(password: string) : Promise<boolean>{
+    const hash = await this.change_hash(password);
+
+    const isMatch = bcrypt.compare(password, hash);
+
+    return isMatch;
+  }
+
+
+  private async change_hash(password: string) : Promise<string>{
+    const hash = await bcrypt.hash(password, passowrd_hash);
+    return hash;
+  }
 }
+
