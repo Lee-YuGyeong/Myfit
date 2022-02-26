b998a3861194f792a9305c022cbd1d34a7b86c13
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2022-02-27 00:20:40 +0900

    spring setting

---
+++ myfit/src/main/java/com/my/myfit/MyfitApplication.java
@@ -0,0 +1,13 @@
+package com.my.myfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfitApplication.class, args);
+	}
+
+}
