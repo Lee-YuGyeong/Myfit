122f0488c692ffb3f6181e402665992ebcc51211
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2022-03-11 16:42:15 +0900

    파일 이동

--- coupon/src/coupons/coupons.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Coupon } from './coupon.entity';
-
-@Module({
-    imports: [TypeOrmModule.forFeature([Coupon])]
-})
-export class CouponsModule {}
--- coupon/src/user_coupons/user_coupons.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserCoupon } from './user_coupon.entity';
-
-@Module({
-    imports: [TypeOrmModule.forFeature([UserCoupon])]
-})
-export class UserCouponsModule {}
